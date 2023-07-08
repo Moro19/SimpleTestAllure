483f85f287b5e36428ee79d3230faa68a44d32e9
Moro <amaru89@yandex.ru>
2023-07-08 13:40:30 +0300

    add

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -4,10 +4,13 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
@@ -36,4 +39,10 @@ public class TestBase {
         Attach.browserConsoleLogs();
         Attach.addVideo();
     }
+    @AfterAll
+    static void close () {
+        closeWebDriver();
+
+    }
+
 }
